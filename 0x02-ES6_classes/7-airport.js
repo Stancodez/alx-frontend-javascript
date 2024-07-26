981ca37dd7b8b14e92c0115888b7c4cacda46c5e
Stanley <stanleyemeke71@gmail.com>
2024-07-26 02:38:48 +0000

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,24 +1,26 @@
-// 7-airport.js
-class Airport {
+export default class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._name = name;
-    this._code = code;
+    this.name = name;
+    this.code = code;
   }
 
-  // Custom toString method to return the airport code
-  toString() {
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
     return this._code;
   }
 }
-
-// Example usage
-const airport = new Airport('Los Angeles International', 'LAX');
-console.log(airport.toString()); // Output: LAX
-
-export default Airport;
