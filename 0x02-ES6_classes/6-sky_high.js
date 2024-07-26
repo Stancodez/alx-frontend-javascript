7f0bc0c8278df6ca7ef8106d2345fc2af35fcea2
Stanley <stanleyemeke71@gmail.com>
2024-07-26 02:36:26 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,28 +1,20 @@
-// 6-sky_high.js
-import Building from './5-building.js';
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('Number of floors must be a number');
-    }
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-
-// Example usage
-const skyscraper = new SkyHighBuilding(20000, 50);
-console.log(skyscraper.sqft); // Output: 20000
-console.log(skyscraper.floors); // Output: 50
-console.log(skyscraper.evacuationWarningMessage()); // Output: Evacuate slowly the 50 floors.
-
-export default SkyHighBuilding;
