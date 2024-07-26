f8f48e36883e41f22211898ac40bc0ccf547b229
Stanley <stanleyemeke71@gmail.com>
2024-07-26 02:41:26 +0000

    Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,30 +1,32 @@
-// 8-holberton_class.js
-class HolbertonClass {
+export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number') {
-      throw new TypeError('Size must be a number');
-    }
-    if (typeof location !== 'string') {
-      throw new TypeError('Location must be a string');
-    }
-    this._size = size;
-    this._location = location;
+    this.size = size;
+    this.location = location;
   }
 
-  // Custom valueOf method to return the size when cast to a number
-  valueOf() {
+  get size() {
     return this._size;
   }
 
-  // Custom toString method to return the location when cast to a string
-  toString() {
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
     return this._location;
   }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
 }
-
-// Example usage
-const holbertonClass = new HolbertonClass(30, 'San Francisco');
-console.log(Number(holbertonClass)); // Output: 30
-console.log(String(holbertonClass)); // Output: San Francisco
-
-export default HolbertonClass;
