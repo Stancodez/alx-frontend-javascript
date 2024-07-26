561739bda1571b47d663f84f197a41e3145d435a
Stanley <stanleyemeke71@gmail.com>
2024-07-26 02:46:33 +0000

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,46 +1,41 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
-  // Getter for brand
   get brand() {
-    return this[_brand];
+    return this._brand;
   }
 
-  // Setter for brand
   set brand(value) {
-    this[_brand] = value;
+    this._brand = value;
   }
 
-  // Getter for motor
   get motor() {
-    return this[_motor];
+    return this._motor;
   }
 
-  // Setter for motor
   set motor(value) {
-    this[_motor] = value;
+    this._motor = value;
   }
 
-  // Getter for color
   get color() {
-    return this[_color];
+    return this._color;
   }
 
-  // Setter for color
   set color(value) {
-    this[_color] = value;
+    this._color = value;
   }
 
-  // Method to clone the car
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
 }
