d64d53478dd49c2250474ba4af8852dbeedf063a
Stanley <stanleyemeke71@gmail.com>
2024-07-31 01:02:52 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,26 +1,6 @@
-function getListStudents() {
-    return [
-        {
-            id: 1,
-            firstName: 'Guillaume',
-            location: 'San Francisco'
-        },
-        {
-            id: 2,
-            firstName: 'James',
-            location: 'Columbia'
-        },
-        {
-            id: 5,
-            firstName: 'Serena',
-            location: 'San Francisco'
-        }
-    ];
-}
-
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
+  }
+  return [];
 }
